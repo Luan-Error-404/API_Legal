831ac679b62cc172898e5a801681d6497296e2aa
Luan-Error-404 <jesus.l@estudante.ifro.edu.br>
2025-11-04 17:42:55 -0400

    aiai

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 import express from "express";
 import pool from './servicos/conexao.js'
+import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno } from "./servicos/retornaConexao_servico.js";
 const app = express();
 const port = 9000;
+
+app.get('/campeonatos', async (req,res) => {
+
+    
+    if(anoInvalido) {
+        campeonatos = await retornaCampeonatos();
+    } else if(!anoInvalido) {
+        if (!isNaN(ano)) {
+        campeonatos = await retornaCampeonatosAno(ano);
+        } else
+        res.status (400).json({ mensagem: "Requisição Inválida: 'ano' deve ser um número." });
+    }
+});
+
+app.get('/campeonatos/:id', async (req, res) => {
+    let campeonato;
+    const id = parseInt(req.params.id);
+    if (!isNaN(id)) {
+        campeonato = await retornaCampeonatosID(id);
+    } else {
+        res.status (400).json({ mensagem: "Requisição Inválida" });
+    }
+
+    if (campeonato.length > 0) {
+        res.json(campeonato);
+        } else {
+        res.status (404).json({ mensagem: "Nenhum campeonato encontrado" });
+    }
+});
+
+
 
 app.listen(port, async () => {
     const data = new Date();
